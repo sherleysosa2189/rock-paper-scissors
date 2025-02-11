f1090b34b6c7060603de504ad730270ed34be672
sherleysosa <sherleysosa2189@gmail.com>
2025-02-11 13:05:15 -0500

    Added code in javascript Added code in Javascript that shows how to play the game with human intervention. Will be fixing it without human intervention

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     const randomNumber = Math.random();
     if (randomNumber < 1 / 3) {
@@ -11,4 +14,43 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice()); 
+console.log(getComputerChoice());
+
+function getHumanChoice() {
+    let userChoice = prompt("Please write rock, paper, scissors;");
+    return userChoice;
+}
+
+console.log(getHumanChoice());
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+
+
+    if (humanChoice === computerChoice) {
+        console.log("It's a tie!");
+    }
+    else if (
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper")
+    ) {
+        humanScore++;
+        console.log(`You win! ${humanChoice.charAt(0) + humanChoice.slice(1)} beats ${computerChoice}.`);
+    } 
+    else {
+        computerScore++;
+        console.log(`You lose! ${computerChoice.charAt(0) + computerChoice.slice(1)} beats ${humanChoice}.`);
+    }
+}
+
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+playRound(humanChoice, computerChoice);
+
+console.log(`Human Score: ${humanScore}`);
+console.log(`Computer Score: ${computerScore}`);
+
+
+
+
